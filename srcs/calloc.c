c5c44dad6729a2676289a6d82ad10be33a04378c
Langlait Corentin <langlaitcorentin@MacBook-Pro.local>
2020-10-11 10:38:15 +0200

    [WIP] Calloc

--- srcs/calloc.c
+++ srcs/calloc.c
@@ -6,17 +6,15 @@
 /*   By: langlaitcorentin <langlaitcorentin@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/03 08:40:04 by clanglai          #+#    #+#             */
-/*   Updated: 2020/10/10 23:06:48 by langlaitcor      ###   ########.fr       */
+/*   Updated: 2020/10/11 10:37:37 by langlaitcor      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_malloc.h"
 
 void	*calloc(size_t count, size_t size) {
-	size_t	idx;
 	void	*alloc;
 	
-	idx = 0;
 	if (count == 0 || size == 0) {
 		return NULL;
 	}
